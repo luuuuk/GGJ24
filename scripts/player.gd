4056e2fe8768218bacf116906938b36dc51c4167
Lukas Schild <luk.schild@t-online.de>
2024-01-26 22:04:42 +0100

    feat: player sprite animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ const MAX_SPEED = 300
 var velocity_percent = Vector2.ZERO
 var acceleration = 30
 
+@onready var _animated_sprite = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,16 +19,21 @@ func _process(delta):
 	
 	if Input.is_action_pressed("up"):
 		velocity_percent = velocity_percent.move_toward(Vector2(0,-1), acceleration * delta)
+		_animated_sprite.play("move_up")
 	elif Input.is_action_pressed("down"):
 		velocity_percent = velocity_percent.move_toward(Vector2(0,1), acceleration * delta)
+		_animated_sprite.play("move_down")
 	elif Input.is_action_pressed("left"):
 		velocity_percent = velocity_percent.move_toward(Vector2(-1,0), acceleration * delta)
+		_animated_sprite.play("move_left")
 	elif Input.is_action_pressed("right"):
 		velocity_percent = velocity_percent.move_toward(Vector2(1,0), acceleration * delta)
+		_animated_sprite.play("move_right")
 	else:
 		velocity_percent = velocity_percent.move_toward(Vector2.ZERO, acceleration * delta)
+		_animated_sprite.stop()
 	
 	velocity = MAX_SPEED * velocity_percent
 	move_and_slide()
 	
-	# Pick somthing up if colliding
+	# Pick something up if colliding
