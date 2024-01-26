5838e7e57a4bbba2068b04d204709cad236d32b2
LauEls <44226646+LauEls@users.noreply.github.com>
2024-01-26 22:08:13 +0100

    added computer puzzle

--- scripts/levelChangeTrigger.gd
+++ scripts/levelChangeTrigger.gd
@@ -13,4 +13,4 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://scenes/main.tscn")
+		get_tree().change_scene_to_file("res://scenes/computer_puzzle/test_zones.tscn")
