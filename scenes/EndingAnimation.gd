30f22f1e96edfe128ca593573b5ffaf99a1879ab
RaquelTirach <raquel.tirach@grenoble-inp.org>
2024-01-28 14:56:50 +0100

    feat: rickroll

--- scenes/EndingAnimation.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-var animation := AnimationPlayer.new()
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	animation.autoplay = true
-	add_child(animation)
